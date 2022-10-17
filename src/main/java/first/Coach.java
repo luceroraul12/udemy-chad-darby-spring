36bf801f83cbc25f456f20ceb84945e92d0ca6e8
Lucero Raul <luceroraul12@gmail.com>
2022-10-17 11:07:14 -0300

    In projects Intellij, xml put in resources for you can use it

--- src/main/java/first/Coach.java
+++ src/main/java/first/Coach.java
@@ -1,3 +1,5 @@
+package first;
+
 public interface Coach {
     String getDailyWorkout();
 }
--- src/main/java/first/MyApp.java
+++ src/main/java/first/MyApp.java
@@ -1,6 +1,8 @@
+package first;
+
 public class MyApp {
     public static void main(String[] args) {
-        Coach theCoach = new BaseballCoach();
+        Coach theCoach = new TrackCoach();
 
         System.out.println(theCoach.getDailyWorkout());
     }
